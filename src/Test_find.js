eaa4b8aefca39721be7eee5a90c09bef7e114a67
leedh909 <leedh909@naver.com>
2021-02-01 13:43:29 +0900

    검색 후 값 가져오기(라이프사이클 알아보기->console.log)

--- src/Test_find.js
+++ src/Test_find.js
@@ -3,6 +3,7 @@ import React from 'react';
 class Test_find extends React.Component {
   constructor(props) {
     super(props);
+    console.log('constructor');
     this.state = {
       items: [],
       select_v: 'ID',
@@ -12,6 +13,7 @@ class Test_find extends React.Component {
   }
 
   componentWillMount() {
+    console.log('componentWillMount');
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((res) => res.json())
       .then((data) =>
@@ -23,6 +25,7 @@ class Test_find extends React.Component {
 
   //검색 목록 값 변경
   selectChange = (e) => {
+    console.log('selectChange');
     this.setState({
       select_v: e.target.value,
     });
@@ -30,36 +33,40 @@ class Test_find extends React.Component {
 
   //검색 값 변경
   inputChange = (e) => {
+    console.log('inputChange');
     this.setState({
       input_v: e.target.value,
     });
   };
   //결과 값 변경
-  resultChange = (e) => {
-    this.setState({
-      result: e.target.value,
-    });
-  };
+  // resultChange = (e) => {
+  //   this.setState({
+  //     result: e.target.value,
+  //   });
+  // };
   //검색버튼 클릭시 이벤트 작성
   select_click = (e) => {
+    console.log('select_click');
     //items를 확인해서 있을경우 없을경우
-
     if (this.state.select_v === 'ID') {
-      console.log(this.state.result);
+      console.log('1번 ' + this.state.result);
       for (let i = 0; i < this.state.items.length; i++) {
         if (
           this.state.items[i].id.toString() === this.state.input_v.toString()
         ) {
           setTimeout(() => {
+            console.log('setTimeout');
             this.setState({
               result:
                 this.state.items[i].id + ' / ' + this.state.items[i].title,
             });
+            console.log('2번 ' + this.state.result);
           }, 0);
         }
-        console.log(this.state.result);
+        // console.log(this.state.result);
       }
       if (this.state.result === '') {
+        console.log('검색값 없을때');
         this.setState({
           result: '검색하신 값이 없습니다.',
         });
@@ -81,6 +88,7 @@ class Test_find extends React.Component {
   };
 
   render() {
+    console.log('render');
     return (
       <div>
         <label>선택: </label>
